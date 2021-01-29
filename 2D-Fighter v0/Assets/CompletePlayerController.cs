6ba02c29508905f2a0ab04577093a3d6f2515796
kdhoka <kdhoka@scu.edu>
2021-01-29 07:07:46 +0700

    Updating jump mechanics
    Fixed previous infinite jump issue, instead added game object for detection of platforms, a layer to identify platforms and necessary code to player controller

--- 2D-Fighter v0/Assets/CompletePlayerController.cs
+++ 2D-Fighter v0/Assets/CompletePlayerController.cs
@@ -8,6 +8,12 @@ public class CompletePlayerController : MonoBehaviour
     public float speed;                //Floating point variable to store the player's movement speed.
     public float jumpForce;
 
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public int extraJumps;
+
     private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private Animator anim;
     private SpriteRenderer sprite;
@@ -27,14 +33,13 @@ public class CompletePlayerController : MonoBehaviour
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical * jumpForce);
+        //Use the  float to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, 0);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
         if(rb2d.velocity.y > 0){
             anim.SetBool("IsJumping", true);
@@ -75,4 +80,23 @@ public class CompletePlayerController : MonoBehaviour
             
         }
     }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+        }
+    }
+
 }
